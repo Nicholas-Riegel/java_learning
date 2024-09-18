ddcf9ace9e43b918b300b0ff7c00032ed42d989e
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-09-18 09:33:05 -0400

    init

---
+++ basics_01/DataTypes01.java
@@ -0,0 +1,21 @@
+public class DataTypes01 {
+
+    public static void main(String[] args) {
+
+        /*
+        DATA TYPES
+        1. String: "hello"
+        2. Integer: byte, short, int, long( add l to number)
+        3. Float: float (add f to number), double 
+        4. Character: char 'c'
+        5. Boolean: boolean
+        */
+
+        // CASTING
+        // byte a = 47;
+        // int b = 14;
+        // a = (byte)b;
+        // System.out.println(a);
+
+    } 
+}
