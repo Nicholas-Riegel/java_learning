176919bdbcebeb040069f97d5631411a1de92ab2
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-09-20 17:14:02 -0400

    .

--- oop_02/j03Encapsulation.java
+++ oop_02/j03Encapsulation.java
@@ -4,18 +4,32 @@ class Human
     private String name;
     private int age;
 
+    // Default Constructor
+    public Human()
+    {
+        this.name = "No name.";
+        this.age = 0;
+    }
+    
+    // Constructor overloading
+    public Human(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    
     // right click here, generate getters and setters
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String a) {
+        name = a;
     }
     public int getAge() {
         return age;
     }
-    public void setAge(int age) {
-        this.age = age;
+    public void setAge(int a) {
+        age = a;
     }
     
 }
@@ -25,6 +39,13 @@ public class j03Encapsulation {
     public static void main(String[] args) {
 
         Human human1 = new Human();
-        // human1.name = "Nick";
+        human1.setName("Nick");
+        human1.setAge(4);
+
+        System.out.println(human1.getName());
+        System.out.println(human1.getAge());
+
+        System.out.println(new Human().getName()); // Anonymous object
+        
     }
 }
