4acd81b3158f88a4b16f7ca23d73d24ceb96d28f
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-09-21 10:52:24 -0400

    packages and compile

---
+++ j05_method_override/Program.java
@@ -0,0 +1,30 @@
+class A
+{
+    public void Show()
+    {
+        System.out.println("In A show.");
+    }
+
+    public void config()
+    {
+        System.out.println("In A config.");
+    }
+}
+
+class B extends A
+{
+    // this will override A's Show method
+    public void Show()
+    {
+        System.out.println("In B show.");
+    }
+}
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        B obj = new B();
+        obj.Show();
+    }
+}
