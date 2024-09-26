ccaeb762a4f884c399d6b22888a350373dd17d7c
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-09-26 19:05:46 -0400

    try with resources

---
+++ j28_try_with_resources/Program.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Program {
+
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("Enter your name.");
+        // BufferedReader br = null;
+
+        // This will close it automatically
+        // instead of using a finally block to close it
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) 
+        {
+    
+            String input = br.readLine();
+
+            System.out.println(input);
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        } 
+        // finally 
+        // {
+
+        //     br.close();
+        // }
+
+        System.out.println("End of program.");
+    }
+}
